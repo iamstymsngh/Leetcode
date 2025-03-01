9e98697a2bc1afaa1399ec00297658a5969cc914
iamstymsngh <iamstymsingh@gmail.com>
2025-03-01 10:17:46 +0530

    69. Sqrt(x)

---
+++ Binary Search/69. Sqrt(x).py
@@ -0,0 +1,14 @@
+class Solution:
+    # TC - O(logn)
+    def mySqrt(self, x: int) -> int:
+        low, high = 1, x
+        while low <= high:
+            mid = low+(high-low)//2
+            square = mid*mid
+            if square == x:
+                return mid
+            elif square < x:
+                low=mid+1
+            else:
+                high=mid-1
+        return high
