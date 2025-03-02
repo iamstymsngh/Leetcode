2ca573695dedf677290c7603b94087b9820171f6
iamstymsngh <iamstymsingh@gmail.com>
2025-03-02 18:24:02 +0530

    1287. Element Appearing More Than 25% In Sorted Array

--- Binary Search/1287. Element Appearing More Than 25% In Sorted Array.py
+++ Binary Search/1287. Element Appearing More Than 25% In Sorted Array.py
@@ -10,4 +10,13 @@ class Solution:
         for key,val in counter.items():
             if val > length / 4:
                 return key
+
+    # TC - O(n)
+    # SC - O(1)
+    def findSpecialInteger2(self, arr: List[int]) -> int:
+        length = len(arr)
+        frequency = length // 4
+        for i in range(length - frequency + 1):
+            if arr[i] == arr[i+frequency]:
+                return arr[i]
                 
