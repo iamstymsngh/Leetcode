2ba5f748c1276a8556640f42e951b54277e3b429
iamstymsngh <iamstymsingh@gmail.com>
2025-03-08 13:48:52 +0530

    876. Middle of the Linked List

---
+++ Linked List/876. Middle of the Linked List.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # TC -> O(n)
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow if slow else None
